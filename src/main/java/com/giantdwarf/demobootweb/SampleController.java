1aa171280428f4d224249779e7c7f912df53acf8
seungin yang <rhfpdk92@naver.com>
2020-04-20 23:36:53 +0900

    Formatter 설정

---
+++ src/main/java/com/giantdwarf/demobootweb/SampleController.java
@@ -0,0 +1,17 @@
+package com.giantdwarf.demobootweb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam("name") Person person){
+        return "hello " + person.getName();
+    }
+
+}
