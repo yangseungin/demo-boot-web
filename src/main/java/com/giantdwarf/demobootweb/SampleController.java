ca19cd27ee2be49c0aa86b0d2a75938403058b8e
seungin yang <rhfpdk92@naver.com>
2020-04-21 14:25:37 +0900

    HandlerInterceptor 구현 및 등록

--- src/main/java/com/giantdwarf/demobootweb/SampleController.java
+++ src/main/java/com/giantdwarf/demobootweb/SampleController.java
@@ -9,6 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SampleController {
 
+    //preHandle 1
+    //preHandle 2
+    //요청처리
+    //postHandle 2 <- postHandler는 역순으로 호출
+    //postHandle 1
+    //afterCompletion 2 <-afterCompletion 역시 역순으로 호출
+    //afterCompletion 1
+
     @GetMapping("/hello")
     public String hello(@RequestParam("id") Person person){
         return "hello " + person.getName();
--- src/main/java/com/giantdwarf/demobootweb/WebConfig.java
+++ src/main/java/com/giantdwarf/demobootweb/WebConfig.java
@@ -1,14 +1,17 @@
 package com.giantdwarf.demobootweb;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-//    @Override
-//    public void addFormatters(FormatterRegistry registry) {
-//        registry.addFormatter(new PersonFormatter());
-//    }
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new GreetingInterceptor()).order(0);
+        registry.addInterceptor(new AnotherInterceptor())
+                .addPathPatterns("/hi")
+                .order(-1);
+    }
 }
