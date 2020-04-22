faf7692b75100dcc66ad0aba12980b354e3f96d0
seungin yang <rhfpdk92@naver.com>
2020-04-22 23:39:21 +0900

    ResourceHandler Registry

--- src/main/java/com/giantdwarf/demobootweb/WebConfig.java
+++ src/main/java/com/giantdwarf/demobootweb/WebConfig.java
@@ -1,8 +1,12 @@
 package com.giantdwarf.demobootweb;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -13,5 +17,13 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(new AnotherInterceptor())
                 .addPathPatterns("/hi")
                 .order(-1);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/mobile/**")
+                .addResourceLocations("classpath:/mobile/")
+                .setCacheControl(CacheControl.maxAge(10, TimeUnit.MINUTES));
+
     }
 }
--- src/test/java/com/giantdwarf/demobootweb/SampleControllerTest.java
+++ src/test/java/com/giantdwarf/demobootweb/SampleControllerTest.java
@@ -1,18 +1,20 @@
 package com.giantdwarf.demobootweb;
 
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -36,6 +38,23 @@ public class SampleControllerTest {
                     .param("id",savedPerson.getId().toString()))
                 .andDo(print())
                 .andExpect(content().string("hello yang"));
+    }
+
+    @Test
+    public void helloStatic() throws Exception{
+        this.mockMvc.perform(get("/index.html"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(Matchers.containsString("hello index")));
+    }
+    @Test
+    public void mobileStatic() throws Exception{
+        this.mockMvc.perform(get("/mobile/index.html"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(Matchers.containsString("hello mobile")))
+                .andExpect(header().exists(HttpHeaders.CACHE_CONTROL));
+
     }
 
 }
